f0f105b370003239fa613f2b41a2de3ec9a1d90f
kbchun <kbchun5712@gmail.com>
2016-09-28 17:06:39 -0700

    All basic requirements tests pass

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,31 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      this.at(0).enqueue();
+    });
+
+    this.on('enqueue', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function(e) {
+      this.remove(this.at(0));
+      if (this.at(0)) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(e) {
+      this.remove(e);
+    });
+    
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -2,6 +2,7 @@
 var AppModel = Backbone.Model.extend({
 
   initialize: function(params) {
+
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
 
@@ -15,6 +16,14 @@ var AppModel = Backbone.Model.extend({
 
     params.library.on('play', function(song) {
       this.set('currentSong', song);
+    }, this);
+
+    params.library.on('enqueue', function(song) {
+      this.get('songQueue').add(song);
+    }, this);
+
+    params.library.on('dequeue', function(song) {
+      this.get('songQueue').remove(song);
     }, this);
   }
 
